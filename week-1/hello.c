86351aa32febec151714be977754b6d7e95eb4bc
Thamid1971 <abdullathamid1971@gmail.com>
2026-07-29 18:45:03 +0600

    :)

--- week-1/hello.c
+++ week-1/hello.c
@@ -1,8 +1,10 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void)
-{
-    string name = get_string("What's your name? ");
+int main(void){
+    string name;
+    name = get_string("What's your name? ");
+
     printf("hello, %s\n", name);
+
 } 
---
+++ week-1/mario-less/mario.c
@@ -0,0 +1,27 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int h;
+    do
+    {
+        h = get_int("Height: ");
+    }
+    while (h <= 0);
+
+    for (int i = 1; i <= h; i++)
+    {
+        for (int j = i; j < h; j++)
+        {
+            printf(" ");
+        }
+        for (int p = 0; p < i; p++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
